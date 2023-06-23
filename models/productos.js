521047fb43cebe4dd9e233d0552e606da980b5b8
Camilo Lavado <93832685+Kroszover@users.noreply.github.com>
2023-06-22 21:09:00 -0400

    Modelos Creados

---
+++ models/productos.js
@@ -0,0 +1,53 @@
+class Producto {
+  constructor(
+    productoId,
+    productoNombre,
+    precioUnitario,
+    unidadesEnStock,
+    descontinuado
+  ) {
+    this._productoId = productoId;
+    this._productoNombre = productoNombre;
+    this._precioUnitario = precioUnitario;
+    this._unidadesEnStock = unidadesEnStock;
+    this._descontinuado = descontinuado;
+  }
+
+  get productoId() {
+    return this._productoId;
+  }
+
+  set productoId(productoId) {
+    this._productoId = productoId;
+  }
+
+  get productoNombre() {
+    return this._productoNombre;
+  }
+
+  set productoNombre(productoNombre) {
+    this._productoNombre = productoNombre;
+  }
+
+  get precioUnitario() {
+    return this._precioUnitario;
+  }
+
+  get unidadesEnStock() {
+    return this._unidadesEnStock;
+  }
+
+  set unidadesEnStock(unidadesEnStock) {
+    this._unidadesEnStock = unidadesEnStock;
+  }
+
+  get descontinuado() {
+    return this._descontinuado;
+  }
+
+  set descontinuado(descontinuado) {
+    this._descontinuado = descontinuado;
+  }
+}
+
+module.exports = Producto;
